a5deee34f9a483c47cc2d5ffc9ae5bf2877ef1fc
Henry <henryah.yeb.5@gmail.com>
2025-03-29 02:56:57 +0000

    save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 import { products } from "./products.js";
 
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -10,6 +10,10 @@ export let cart = [
     quantity: 1,
   },
 ];
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -26,6 +30,7 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -36,4 +41,5 @@ export function removeFromCart(productId) {
     }
   });
   cart = newCart;
+  saveToStorage();
 }
